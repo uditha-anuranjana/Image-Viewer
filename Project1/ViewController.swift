fa4ed83635b9982886a67c26019d5d99eee38d0a
uditha <xxudithaxx@gmail.com>
2021-02-08 17:06:50 +0530

    image names changed

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -29,7 +29,7 @@ class ViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
-        cell.textLabel?.text = pictures[indexPath.row]
+        cell.textLabel?.text = "Picture \(indexPath.row + 1) of \(pictures.count)"
         return cell
         
     }
