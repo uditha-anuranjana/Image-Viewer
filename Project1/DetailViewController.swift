240e4d48c630bf4fa9872916335a5f6202546e39
uditha <xxudithaxx@gmail.com>
2021-02-08 17:03:37 +0530

    addedsorted the images

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -1,29 +1,29 @@
-//
-//  DetailViewController.swift
-//  Project1
-//
-//  Created by 4Axis on 2/8/21.
-//
-
 import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage : String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
 
-        // Do any additional setup after loading the view.
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
     }
-    */
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
 
 }
